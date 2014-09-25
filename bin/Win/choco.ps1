73c6265163ca9eeac0db42c43c81dba902a4f0bb
Rahul Gopinath <rahul@puppetlabs.com>
2014-09-25 12:25:26 -0700

    (WIN) make the scripts exit on error

--- bin/Win/choco.ps1
+++ bin/Win/choco.ps1
@@ -1,2 +1,4 @@
+$ErrorActionPreference = "Stop"
 $env:PATH = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 & C:\ProgramData\chocolatey\bin\choco.exe $args
+exit $lastexitcode
